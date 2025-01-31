72a004deb002c797f7f2221f033351b1897321ae
arielgee <ariel.hazard@gmail.com>
2025-01-31 23:44:04 +0200

    Update content.js

--- content.js
+++ content.js
@@ -2,23 +2,19 @@
 
 (function() {
 
-	const BUTTON_NAMES_TO_CLICK = [
-		"View more comments",
+	const BUTTON_TEXT_TO_CLICK = [
 		"more replies",
-		"1 more reply"
+		"1 more reply",
+		"View more comments",
 	];
-	// const SVG_PLUS_PATH = "M10.625 9.375H14v1.25h-3.375V14h-1.25v-3.375H6v-1.25h3.375V6h1.25v3.375ZM20 10A10 10 0 1 1 10 0a10.011 10.011 0 0 1 10 10Zm-1.25 0A8.75 8.75 0 1 0 10 18.75 8.76 8.76 0 0 0 18.75 10Z";
 
 	initialization();
 
 	////////////////////////////////////////////////////////////////////////////////////
 	function initialization() {
-		clickAllButtons();
-		observeBodyMutation();
-	}
 
-	////////////////////////////////////////////////////////////////////////////////////
-	function observeBodyMutation() {
+		clickAllButtons();
+
 		const observer = new MutationObserver(clickAllButtons);
 		observer.observe(document.body, { childList: true, subtree: true });
 	}
@@ -32,7 +28,7 @@
 	function clickButton(button) {
 
 		const isVisible = (button.offsetParent !== null);
-		const isClickable = BUTTON_NAMES_TO_CLICK.some((name) => button.innerText.includes(name));	// || button.innerHTML.includes(SVG_PLUS_PATH);
+		const isClickable = BUTTON_TEXT_TO_CLICK.some((t) => button.innerText.includes(t));
 
 		if( isVisible && isClickable ) {
 			button.click();
